b7ea370ec9ab26a940ea88ef029df61eec40a5ec
sims45@gmail.com <sims45@gmail.com>
2016-11-29 20:24:33 -0500

    updated tests

--- spec/features/create_route.rb
+++ spec/features/create_route.rb
@@ -1,3 +1,7 @@
 require 'rails_helper'
-describe "to create a route"
-it 
+describe "to create a route", :type=> :feature do
+it 'I should create a route for a user'
+visit '/'
+expect(page).to have_content('Create a Route')
+click_link('Create a Route')
+end
